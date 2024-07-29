dcc63cbcd39aa0799d390a8cfb9c22a46a6cef7f
Diluk Mihiranga <dilukedu@gmail.com>
2024-07-29 16:39:55 +0530

    color changed

--- lib/utils/colors/colors.dart
+++ lib/utils/colors/colors.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 class AppColors {
   final white = const Color(0xFFFFFFFF);
   final black = const Color(0xFF000000);
-  final primary = const Color.fromARGB(255, 208, 8, 68);
-  final secondary = const Color.fromARGB(255, 252, 42, 105);
+  final primary = const Color.fromARGB(255, 255, 234, 0);
+  final secondary = const Color.fromARGB(255, 255, 217, 0);
 
   const AppColors();
 }
